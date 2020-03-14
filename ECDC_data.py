e717c3a36025e0700cadb50509ca6844d9181557
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2020-03-14 13:38:49 +0100

    intial script

---
+++ ECDC_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# import geopandas as gpd
+import pandas as pd
+
+import os
+
+
+data = pd.read_excel(os.path.join('data', 'ECDC', 'COVID-19-geographic-disbtribution-worldwide-2020-03-14.xls'))
