1737fdefbe6a847334b59ca84ff527f03fba86d9
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2020-03-17 23:20:31 +0100

    change between metrics

--- CSSEGIS.py
+++ CSSEGIS.py
@@ -17,46 +17,47 @@ from ParameterEstimation import parameterestimation, addmeasures
 from SIR import SIRmodel
 
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 data = load_covid19_data()
 
-plotdata(data['confirmed_nzd']['data'], title='confirmed (normalized)', ylabel='confirmend/capita (%)')
+# plotdata(data['confirmed_nzd']['data'], title='confirmed (normalized)', ylabel='confirmend/capita (%)')
 # plotdata(data['deaths_nzd']['data'], title='deaths (normalized)', ylabel='death/capita (%)')
-plotdata((data['deaths_nzd']['data']/data['confirmed_nzd']['data']).replace([np.inf, -np.inf], np.nan), title='deaths per confirmed', ylabel='death/confirmed')
+# plotdata((data['deaths_nzd']['data']/data['confirmed_nzd']['data']).replace([np.inf, -np.inf], np.nan), title='deaths per confirmed', ylabel='death/confirmed')
 
-# diff/(confirmend-diff-death-recovered)
-plotdata(data['confirmed_nzd']['data'].diff().div((data['confirmed_nzd']['data']-data['confirmed_nzd']['data'].diff()-data['deaths_nzd']['data']-data['recovered_nzd']['data'])), title='spreading rate per day', ylabel='spreading rate/day', smoothdays=2)
-
-
-(data['confirmed_nzd']['data']-data['recovered_nzd']['data']-data['deaths_nzd']['data'])['KOR']
+# # diff/(confirmend-diff-death-recovered)
+# plotdata(data['confirmed_nzd']['data'].diff().div((data['confirmed_nzd']['data']-data['confirmed_nzd']['data'].diff()-data['deaths_nzd']['data']-data['recovered_nzd']['data'])), title='spreading rate per day', ylabel='spreading rate/day', smoothdays=2)
 
 
-parameter = parameterestimation(data['confirmed']['data'], 'ITA', output=True)
-SIRmodel(data, 'ITA', parameter, forecast=300, output=True)
-parameter = addmeasures(parameter, datetime(2020, 3, 8), 0.6)
-SIRmodel(data, 'ITA', parameter, forecast=300, output=True)
-
-parameter = parameterestimation(data['confirmed']['data'], 'AUT', output=True)
-SIRmodel(data, 'AUT', parameter, forecast=300, output=True)
-parameter = addmeasures(parameter, datetime(2020, 3, 8), 0.6)
-SIR_data = SIRmodel(data, 'AUT', parameter, forecast=300, output=True)
+# (data['confirmed_nzd']['data']-data['recovered_nzd']['data']-data['deaths_nzd']['data'])['KOR']
 
 
+# parameter = parameterestimation(data['confirmed']['data'], 'ITA', output=True)
+# SIRmodel(data, 'ITA', parameter, forecast=300, output=True)
+# parameter = addmeasures(parameter, datetime(2020, 3, 8), 0.6)
+# SIRmodel(data, 'ITA', parameter, forecast=300, output=True)
+
+# parameter = parameterestimation(data['confirmed']['data'], 'AUT', output=True)
+# SIRmodel(data, 'AUT', parameter, forecast=300, output=True)
+# parameter = addmeasures(parameter, datetime(2020, 3, 8), 0.6)
+# SIR_data = SIRmodel(data, 'AUT', parameter, forecast=300, output=True)
 
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
-app.layout = html.Div([html.Div([html.H1("Demographic Data by Country")],
+app.layout = html.Div([html.Div([html.H1("Covid-19 data by country")],
                                 style={'textAlign': "center", "padding-bottom": "30"}),
                        html.Div([html.Span("Metric to display : ", className="six columns",
                                            style={"text-align": "right", "width": "40%", "padding-top": 10}),
-                                 dcc.Dropdown(id="value-selected", value='lifeExp',
-                                              options=[{'label': "Population ", 'value': 'pop'},
-                                                       {'label': "GDP Per Capita ", 'value': 'gdpPercap'},
-                                                       {'label': "Life Expectancy ", 'value': 'lifeExp'}],
+                                 dcc.Dropdown(id="value-selected", value='confirmed',
+                                              options=[{'label': "population", 'value': 'pop'},
+                                                       {'label': "infected", 'value': 'confirmed'},
+                                                       {'label': "infected normalizied", 'value': 'confirmed_nzd'}],
                                               style={"display": "block", "margin-left": "auto", "margin-right": "auto",
                                                      "width": "70%"},
                                               className="six columns")], className="row"),
@@ -68,23 +69,23 @@ app.layout = html.Div([html.Div([html.H1("Demographic Data by Country")],
     dash.dependencies.Output("my-graph", "figure"),
     [dash.dependencies.Input("value-selected", "value")]
 )
-def update_figure(selected):
 
+def update_figure(selected):
     def title(text):
         if text == "pop":
-            return "Poplulation (million)"
+            return "Population (million)"
         elif text == "gdpPercap":
             return "GDP Per Capita (USD)"
         else:
             return "Life Expectancy (Years)"
-    trace = go.Choropleth(locations=data['confirmed_nzd']['data'].T.index, # Spatial coordinates
-                          z = data['confirmed_nzd']['data'].T[date.today()-timedelta(days=1)], # Data to be color-coded
-                          locationmode = 'ISO-3', # set of locations match entries in `locations`
-                          colorscale = 'Reds',
-                          colorbar_title = "Millions USD",)
+    trace = go.Choropleth(locations=data[selected]['data'].T.index,  # Spatial coordinates
+                          z=data[selected]['data'].T[date.today()-timedelta(days=1)],  # Data to be color-coded
+                          locationmode='ISO-3',  # set of locations match entries in `locations`
+                          colorscale='Reds',
+                          colorbar_title="confirmed_nzd",)
     return {"data": [trace],
-            "layout": go.Layout(title=title(selected),height=800,geo={'showframe': False,'showcoastlines': False,
-                                                                      'projection': {'type': "miller"}})}
+            "layout": go.Layout(title=title(selected), height=800, geo={'showframe': False, 'showcoastlines': False, 'projection': {'type': "miller"}})}
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
