fb120b059fa18997d8ab7496d4817ec1d24a884b
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2020-03-19 23:34:02 +0100

    update

--- CSSEGIS.py
+++ CSSEGIS.py
@@ -12,7 +12,6 @@ import json
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 import plotly.express as px
 
 from DataPreparation import load_covid19_data
