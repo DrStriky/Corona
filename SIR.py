8e79382ba17105ae0da775a443016912aaf896e5
Florian Pribahsnik <Florian.Pribahsnik@gmx.at>
2020-03-21 00:36:29 +0100

    updated label

--- SIR.py
+++ SIR.py
@@ -5,10 +5,11 @@ Created on Tue Mar 17 08:33:58 2020
 @author: pribahsn
 """
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-from datetime import date, timedelta
-from scipy.integrate import ode
+from datetime import timedelta
+from scipy.integrate import odeint
 
 
 def SIRmodel(data, country, parameter, output=False, forecast=600):
@@ -24,10 +25,10 @@ def SIRmodel(data, country, parameter, output=False, forecast=600):
     t = np.linspace(0, forecast-1, forecast)
 
     dummy = odeint(deriv, y0, t, args=(data['population'].at[country], parameter['betagamma'].iloc[0]['beta'], parameter['betagamma'].iloc[0]['gamma']))
-    SIR_data_regular = pd.DataFrame(columns=['S', 'I', 'R'], index=[parameter['t0'] + timedelta(days=x) for x in range(forecast)], data=dummy)
+    SIR_data_regular = pd.DataFrame(columns=['S0', 'I0', 'R0'], index=[parameter['t0'] + timedelta(days=x) for x in range(forecast)], data=dummy)
 
-    SIR_data_curefew = pd.DataFrame(columns=['S0', 'I0', 'R0'], index=[parameter['t0'] + timedelta(days=x) for x in range(forecast)])
-    SIR_data_curefew.loc[parameter['t0']] = {'S': data['population'].at[country], 'I': parameter['I0'], 'R': 0, 'S0': data['population'].at[country], 'I0': parameter['I0'], 'R0': 0}
+    SIR_data_curefew = pd.DataFrame(columns=['S', 'I', 'R'], index=[parameter['t0'] + timedelta(days=x) for x in range(forecast)])
+    SIR_data_curefew.loc[parameter['t0']] = {'S': data['population'].at[country], 'I': parameter['I0'], 'R': 0}
     for i in range(len(parameter['betagamma'])):
         y0 = tuple(SIR_data_curefew.loc[parameter['betagamma'].index[i]].values)
         if i != len(parameter['betagamma'])-1:
@@ -37,7 +38,7 @@ def SIRmodel(data, country, parameter, output=False, forecast=600):
             forecast_step = (parameter['betagamma'].index[0]+timedelta(days=forecast)-parameter['betagamma'].index[i]).days
             t = np.linspace(0, forecast_step, forecast_step+1)
         dummy = odeint(deriv, y0, t, args=(data['population'].at[country], parameter['betagamma'].iloc[i]['beta'], parameter['betagamma'].iloc[i]['gamma']))
-        dummy = pd.DataFrame(columns=['S0', 'I0', 'R0'], index=[parameter['betagamma'].index[i] + timedelta(days=x) for x in range(forecast_step+1)], data=dummy)
+        dummy = pd.DataFrame(columns=['S', 'I', 'R'], index=[parameter['betagamma'].index[i] + timedelta(days=x) for x in range(forecast_step+1)], data=dummy)
         SIR_data_curefew.update(dummy)
 
     SIR_data = pd.concat([SIR_data_regular, SIR_data_curefew], axis=1, join='inner')
@@ -54,33 +55,3 @@ def SIRmodel(data, country, parameter, output=False, forecast=600):
         plt.show()
 
     return SIR_data
-
-
-import numpy as np
-from scipy.integrate import odeint
-import matplotlib.pyplot as plt
-
-# Total population, N.
-N = 1000
-# Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
-# Everyone else, S0, is susceptible to infection initially.
-S0 = N - I0 - R0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10 
-# A grid of time points (in days)
-t = np.linspace(0, 160, 160)
-
-# The SIR model differential equations.
-def deriv(y, t, N, beta, gamma):
-    S, I, R = y
-    dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
-    dRdt = gamma * I
-    return dSdt, dIdt, dRdt
-
-# Initial conditions vector
-y0 = S0, I0, R0
-# Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma))
-S, I, R = ret.T
